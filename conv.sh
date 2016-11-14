344a492a2a1cafcc7cae4ce4337efb8890df5838
chronarion <crestfall@gmail.com>
2016-11-14 13:25:28 -0800

    Add files via upload

--- conv.sh
+++ conv.sh
@@ -1,1 +1,6 @@
-avconv -y -framerate 90 -qscale 1 -i "cursor_pattern_%06d.png" video.webm -t 4
+#avconv -y -framerate 90 -qmax 1 -qscale 1 -i "cursor_pattern_%06d.png" video.webm -t 4
+
+avconv -y -framerate 90 -i "cursor_pattern_%06d.png" -c:v h264 -crf 1 out.mov
+
+rm out.zip
+zip out.zip *.png
